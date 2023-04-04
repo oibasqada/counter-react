d15f2da7bed4b56e98b86ee79d4de175fdba505b
oibasqada <tumarbekovmax@gmail.com>
2023-04-04 16:44:58 +0600

    trying to figure out how does react works

--- counter/src/App.js
+++ counter/src/App.js
@@ -1,9 +1,27 @@
-import React from "react";
-// import Header from "./components/Header";
-
+import React, {useState} from "react";
 
 const App = () => {
-  return ('');
+  const [counter, setCounter] = useState(0)
+    const [value, setValue] = useState(0)
+    const [result, setResult] = useState(0)
+
+  const handleIncrement = () => {
+    setResult(counter + value)
+  }
+
+  const handleDecrement = () => {
+    setResult(counter - value)
+  }
+
+  return (
+      <div className={'math'}>
+        <h3>{result}</h3>
+          <input type="number" onChange={(e) => setValue(Number(e.target.value))}/>
+          <input type="number" onChange={(e) => setCounter(Number(e.target.value))}/>
+        <button onClick={handleIncrement}>+</button>
+        <button onClick={handleDecrement}>-</button>
+      </div>
+  );
 };
 
 export default App;
